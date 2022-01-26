2297d901a6bd1f883b234ff3de6a8420f8b6235d
Raúl Cabezas González <raul.29cg@gmail.com>
2022-01-26 20:28:52 +0100

    Adding utils

---
+++ utils/enable_if.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   enable_if.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rcabezas <rcabezas@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/26 20:26:04 by rcabezas          #+#    #+#             */
+/*   Updated: 2022/01/26 20:27:46 by rcabezas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+namespace ft
+{
+	template<bool Cond, class T = void>
+	struct enable_if {};
+	template<class T>
+	struct enable_if<true, T>
+	{
+		typedef T type;
+	};
+};
---
+++ utils/lexicographical_compare.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexicographical_compare.hpp                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rcabezas <rcabezas@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/26 20:17:14 by rcabezas          #+#    #+#             */
+/*   Updated: 2022/01/26 20:18:44 by rcabezas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+
+namespace ft
+{
+	template <class InputIterator1, class InputIterator2>
+	bool lexicographical_compare(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2)
+	{
+		while (first1 != last1)
+		{
+			if (first2 == last2 || *first2 < *first1)
+				return false;
+			else if (*first1 < *first2)
+				return true;
+			++first1;
+			++first2;
+		}
+		return (first2 != last2);
+	}
+}
